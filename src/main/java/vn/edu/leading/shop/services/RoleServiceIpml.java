c787c58560b28d08245ba4e14127ba044a416d7f
yen-pham <yenpham123456987@gmail.com>
2019-04-13 15:53:52 +0700

    config heroku

---
+++ src/main/java/vn/edu/leading/shop/services/RoleServiceIpml.java
@@ -0,0 +1,59 @@
+package vn.edu.leading.shop.services;
+
+import org.springframework.stereotype.Service;
+import vn.edu.leading.shop.models.RoleModel;
+import vn.edu.leading.shop.models.UserModel;
+import vn.edu.leading.shop.repositories.RoleRepository;
+import vn.edu.leading.shop.repositories.UserRepository;
+
+import java.util.List;
+
+@Service
+public class RoleServiceIpml implements RoleService{
+
+    private final RoleRepository roleRepository;
+
+    public RoleServiceIpml(RoleRepository roleRepository) {
+        this.roleRepository = roleRepository;
+    }
+
+    @Override
+    public List<RoleModel> findAll() {
+        return roleRepository.findAll();
+    }
+
+    @Override
+    public List<RoleModel> search(String term) {
+        return roleRepository.findByNameContaining(term);
+    }
+
+    @Override
+    public RoleModel findById(Long id) {
+        return roleRepository.findById(id).get();
+    }
+
+    @Override
+    public boolean update(RoleModel role) {
+        RoleModel roleModel = roleRepository.findById(role.getId()).orElse(null);
+        if (roleModel == null)
+            return false;
+        roleRepository.save(role);
+        return true;
+    }
+
+    @Override
+    public void save(RoleModel role) {
+        roleRepository.save(role);
+    }
+
+    @Override
+    public boolean delete(Long id) {
+        RoleModel roleModel = roleRepository.findById(id).orElse(null);
+        if (roleModel == null)
+            return false;
+        roleRepository.delete(roleModel);
+        return true;
+    }
+
+
+}
